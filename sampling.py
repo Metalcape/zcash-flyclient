a09c14c3ef496869e5de173df91da0de6deac5d1
Metalcape <dario.capecchi.m@gmail.com>
2025-11-06 12:23:23 +0100

    Derive parameters for non interactive sampling

--- sampling.py
+++ sampling.py
@@ -40,41 +40,34 @@ def sample(n: int, min: int, max: int, delta: float):
     scaled_distrib = min + distrib * (max - min)
     return scaled_distrib
 
-def blocks_to_sample(activation_height: int, chaintip: int, c: float, L: int) -> list[int]:
-    # probability of failure is bounded by 2**(-lambda)
+def derive_parameters(N: int, L: int, c: float, non_interactive: bool = False) -> tuple[float, int]:
+     # probability of failure is bounded by 2**(-lambda)
     LAMBDA = 50
-    # n = chain length
-    N = chaintip
-    # C = attacker success probability
-    # L = estimate of chain difficulty
+    # N = chain length, or total cumulative difficulty (difficulty-aware case)
+    # c = attacker success probability
+    # L = amount of blocks (or total difficulty) that is always sampled
     # L = delta*n = c**k * n
-    # L default is set to the usual size of the non finalized state after sync
-    DELTA = L/N
-    K = math.log(DELTA, c)
+    delta = L/N
+    k = math.log(delta, c)
 
-    m = math.ceil(LAMBDA / math.log(1 - (1 / math.log(DELTA, c)), 0.5))
+    # Security property (differs in the non interactive case)
+    if non_interactive:
+        m = math.ceil((LAMBDA - math.log(c * N, 0.5)) / math.log(1 - (1 / math.log(delta, c)), 0.5))
+        p_max = (1 - (1/k)) ** m
+        assert p_max <= (2 ** (-LAMBDA)) / (c * N)
+    else:
+        m = math.ceil(LAMBDA / math.log(1 - (1 / math.log(delta, c)), 0.5))
+        p_max = (1 - (1/k)) ** m
+        assert p_max <= 2 ** (-LAMBDA)
+    return (delta, m)
 
-    # Security property
-    p_max = (1 - (1/K)) ** m
-    assert p_max <= 2 ** (-LAMBDA)
-
+def blocks_to_sample(activation_height: int, chaintip: int, c: float, L: int, non_interactive: bool = False) -> list[int]:
+    delta, m = derive_parameters(chaintip, L, c, non_interactive)
     deterministic = [i for i in range(chaintip - L, chaintip)]
-    random = sample(m, activation_height, chaintip - L, DELTA)
+    random = sample(m, activation_height, chaintip - L, delta)
     return np.concatenate((random, np.asarray(deterministic, dtype=np.float64))).round().astype(int).tolist()
 
-def difficulty_to_sample(min_work: int, chaintip_total_work: int, c: float, L: int) -> list[int]:
-    LAMBDA = 50
-    # n equals the total cumulative difficulty in this case
-    N = chaintip_total_work
-    # L in this case is the fraction of difficulty that is always sampled
-    DELTA = L/N
-    K = math.log(DELTA, c)
-
-    m = math.ceil(LAMBDA / math.log(1 - (1 / math.log(DELTA, c)), 0.5))
-
-    # Security property
-    p_max = (1 - (1/K)) ** m
-    assert p_max <= 2 ** (-LAMBDA)
-
-    random = sample(m, min_work, chaintip_total_work - L, DELTA)
+def difficulty_to_sample(min_work: int, chaintip_total_work: int, c: float, L: int, non_interactive: bool = False) -> list[int]:
+    delta, m = derive_parameters(chaintip_total_work, L, c, non_interactive)
+    random = sample(m, min_work, chaintip_total_work - L, delta)
     return random.round().astype(int).tolist()
