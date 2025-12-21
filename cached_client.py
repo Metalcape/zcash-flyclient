c491965fa88d29327f95f9122f4dee9e8ffe070d
Metalcape <dario.capecchi.m@gmail.com>
2025-12-21 01:30:02 +0100

    client: add cached client for offline execution

---
+++ cached_client.py
@@ -0,0 +1,129 @@
+from zcash_client import *
+import pandas as pd
+import asyncio
+import bisect
+import json
+import sys
+import os
+
+BLOCKCHAININFO = "experiments/blockchaininfo.json"
+HEADERS = "experiments/headers.csv"
+HEADERS_BIN = "experiments/headers_bin.csv"
+NODES_BIN = "experiments/nodes.csv"
+DIFFMAP = "experiments/diffmap.csv"
+
+class CachedClient(ZcashClient):
+    _bin_node_cache: dict[str, dict[int, str]] = {}
+    _bin_header_cache: dict[int, str] = {}
+    _header_cache: dict[int, dict] = {}
+    _diff_cache: dict[int, int] = {}
+    _blockchaininfo = {}
+    _cache_ready = False
+
+    def __init__(self, user: str, password: str, port: int, host: str, persistent: bool = False):
+        super(CachedClient, self).__init__(user, password, port, host, persistent)
+        self._build_cache()
+       
+    @classmethod
+    def _build_cache(cls):
+        if cls._cache_ready:
+            return
+        
+        print("Reading files for caching...")
+        headers = pd.read_csv(HEADERS)
+        print("Headers file loaded")
+        headers_bin = pd.read_csv(HEADERS_BIN)
+        print("Binary headers file loaded")
+        nodes_bin = pd.read_csv(NODES_BIN)
+        print("Nodes file loaded")
+        diffmap = pd.read_csv(DIFFMAP)
+        print("Difficulty map loaded")
+
+        print("Building header cache")
+        cls._header_cache = { 
+            k: json.loads(v) for k, v in zip(headers['height'], headers['header'], strict=True)
+        }
+        print("Building binary header cache")
+        cls._bin_header_cache = {
+            k: v for k, v in zip(headers_bin['height'], headers_bin['header'], strict=True)
+        }
+
+        upgrades = [
+            ('heartwood', 903000), 
+            ('canopy', 1046400), 
+            ('nu5', 1687104), 
+            ('nu6', 2726400)
+        ]
+        
+        for name, _ in upgrades:
+            df = nodes_bin.query("upgrade==@name")
+            print(f"Building node cache for {name}")
+            cls._bin_node_cache[name] = {k: v for k, v in zip(df['id'], df['node'], strict=True)}
+        
+        print("Building difficulty cache")
+        cls._diff_cache = {
+            int(k): int.from_bytes(bytes.fromhex(v), 'big') for k, v in zip(diffmap['height'], diffmap['total_work'])
+        }
+
+        with open(BLOCKCHAININFO, 'r') as infile:
+            cls._blockchaininfo = json.load(infile)
+        
+        cls._cache_ready = True
+    
+    async def download_header(self, height: int, verbose: bool):
+        if verbose:
+            return self._header_cache[height]
+        else:
+            return self._bin_header_cache[height]
+        
+    async def download_node(self, network_upgrade: str, index: int, verbose: bool):
+        if verbose:
+            return await ZcashClient.download_node(network_upgrade, index, verbose)
+        else:
+            return self._bin_node_cache[network_upgrade][index]
+
+    async def download_headers_parallel(self, block_heights: list[int], verbose: bool = True):
+        if verbose:
+            return [self._header_cache[h] for h in block_heights]
+        else:
+            return [self._bin_header_cache[h] for h in block_heights]
+        
+    async def download_nodes_parallel(self, network_upgrade: str, nodes: list[int], verbose: bool):
+        if verbose:
+            return await ZcashClient.download_nodes_parallel(network_upgrade, nodes, verbose)
+        else:
+            return [self._bin_node_cache[network_upgrade][i] for i in nodes]
+
+    async def get_first_blocks_with_total_work(self, difficulties: list[str]):
+        diff_list = sorted([int.from_bytes(bytes.fromhex(d), 'big') for d in difficulties])
+        return [
+            {'height': self.find_height(d)} for d in diff_list
+        ]
+
+    def find_height(self, target_work: int) -> int:
+        i = bisect.bisect_left(self._diff_cache.values(), target_work)
+        if i >= len(self._diff_cache.values()):
+            raise ValueError(f"No height found with work >= {target_work}")
+        return self._diff_cache.values()[i]   
+    
+    async def send_command(self, method: str, params: list[str] = None):
+        if method == 'getblockchaininfo':
+            return self._blockchaininfo
+        else:
+            return await ZcashClient.send_command(self, method, params)
+
+async def main():
+    cmd = sys.argv[1]
+    params = sys.argv[2:len(sys.argv)]
+    print(params)
+
+    # async with ZcashClient("flyclient", "", 8232, "127.0.0.1") as client:
+    async with CachedClient.from_conf(CONF_PATH) as client:
+        result = await client.send_command(cmd, params)
+
+        if result:
+            response = json.dumps(result, indent=4)
+            print(response)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ download_data.py
@@ -0,0 +1,73 @@
+from zcash_client import ZcashClient, CONF_PATH
+from gen_samples import ACTIVATION_HEIGHT, CHAINTIP
+from zcash_mmr import Tree
+import pandas as pd
+import asyncio
+import json
+import os
+
+HEADERS = "experiments/headers.csv"
+HEADERS_BIN = "experiments/headers_bin.csv"
+NODES_BIN = "experiments/nodes.csv"
+STEP = 15000
+
+async def gen_header_cache(file_path: str, verbose: bool):
+    print(f"Generating file: {file_path}")
+    heights = [h for h in range(ACTIVATION_HEIGHT, CHAINTIP+1)]
+    h_ranges = [heights[i:i+STEP] for i in range(0, len(heights), STEP)]
+    headers = []
+    async with ZcashClient.from_conf(CONF_PATH) as client:
+        for r in h_ranges:
+            headers += (await client.download_headers_parallel(r, verbose))
+            progress = len(headers)
+            total = len(heights)
+            print(f"{progress} / {total} ({(progress/total * 100):.1f}%)")
+        df = pd.DataFrame({
+            'height': heights,
+            'header': [json.dumps(i) for i in headers] if verbose else headers
+        })
+    df.to_csv(file_path, index=False)
+
+async def gen_node_cache(file_path: str):
+    print(f"Generating file: {file_path}")
+    async with ZcashClient.from_conf(CONF_PATH) as client:
+        upgrades = [
+            ('heartwood', 903000), 
+            ('canopy', 1046400), 
+            ('nu5', 1687104), 
+            ('nu6', 2726400)
+        ]
+
+        last_blocks = [n for _, n in upgrades[1:]] + [CHAINTIP]
+        df_dict : dict[str, pd.DataFrame] = {}
+        for (name, height), last in zip(upgrades, last_blocks, strict=True):
+            print(f"Processing nodes for {name}")
+            mmr = Tree([], height)
+            last_node = mmr.node_index_of_block(last)
+            ids = [i for i in range(0, last_node)]
+            id_ranges = [ids[i:i+STEP] for i in range(0, len(ids), STEP)]
+            nodes = []
+            for r in id_ranges:
+                # print(f"Downloading node range {(r[0], r[-1])}")
+                nodes += (await client.download_nodes_parallel(name, r, False))
+                progress = len(nodes)
+                total = len(ids)
+                print(f"{progress} / {total} ({(progress/total * 100):.1f}%)")
+            df_dict[name] = pd.DataFrame({
+                'upgrade': name,
+                'id': ids,
+                'node': nodes
+            })
+        df = pd.concat(df_dict.values())
+    df.to_csv(file_path, index=False)
+
+async def main():
+    if not os.path.isfile(HEADERS):
+        await gen_header_cache(HEADERS, True)
+    if not os.path.isfile(HEADERS_BIN):
+        await gen_header_cache(HEADERS_BIN, False)
+    if not os.path.isfile(NODES_BIN):
+        await gen_node_cache(NODES_BIN)
+
+if __name__ == '__main__':
+    asyncio.run(main())
